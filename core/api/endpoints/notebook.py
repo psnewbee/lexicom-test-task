b15bb75f89240b40dde28ad31832d9364ff146f3
psnewbee <savlovp@mail.ru>
2023-06-15 10:43:07 +0300

    Altered response type for write_data endpoint

--- core/api/endpoints/notebook.py
+++ core/api/endpoints/notebook.py
@@ -1,6 +1,7 @@
 from core.services import redis_service
 from core.models import ContactInfoModel, AddressInfoModel
-from fastapi import APIRouter, HTTPException, Response
+from fastapi import APIRouter, HTTPException, responses
+
 from typing import Any
 
 
@@ -8,8 +9,10 @@ router: APIRouter = APIRouter()
 
 @router.get("/check_data", status_code=200)
 async def check_info(phone: str) -> Any:
+    
     if not await redis_service.exists(phone):
         raise HTTPException(detail='Data associated with provided phone number was not found', status_code=404)
+    
     redis_data: dict = await redis_service.hgetall(phone)
     address_info: AddressInfoModel = AddressInfoModel(**redis_data)
     return address_info
@@ -17,13 +20,15 @@ async def check_info(phone: str) -> Any:
 
 @router.post("/write_data", status_code=200)
 async def add_info(input_data: ContactInfoModel) -> Any:
+    
     input_data = input_data.dict()
     phone = input_data.pop('phone')
 
-    db_record_exist: bool = bool(await redis_service.exists(phone))
+    db_record_exist = bool(await redis_service.exists(phone))
     await redis_service.hmset(name=phone, mapping=input_data)
     
     if db_record_exist:
-        return Response(content="Address record updated", status_code=200)
-    return Response(content="Address record created", status_code=201)
+        return responses.PlainTextResponse(content="Address record updated", status_code=200)
+    
+    return responses.PlainTextResponse(content="Address record created", status_code=201)
     
